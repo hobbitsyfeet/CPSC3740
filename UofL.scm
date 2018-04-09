97436349d3fd80d1ebf83f0df92e148287bdf736
Justin <lego.man.123@hotmail.com>
2018-04-09 17:07:40 -0600

    Added function identifiers
    #definefuncf goes to its own subfunction
    #definevari goes to its own subfunction

--- UofL.scm
+++ UofL.scm
@@ -1,11 +1,12 @@
 #lang racket
 (require parser-tools/lex)
 
-
+;Global Variables
 (define quit "exit")
 (define globalFuncStack '()) ;stack of functions
-(define globalVariStack '()) ;stack of Variables
+(define globalVariStack '()) ;stack of Variables '((var1 val1) (var2 val2) ... (varn valn))
 
+;Clears both global Stacks
 (define (ClearStacks)
   (display "Clearing Functions and Variables ")
   (set! globalFuncStack '()) ;empties global functions
@@ -17,13 +18,20 @@
 (define input "") ;input
   (display "UofL>")
   (set! input (read-line));reads input
-  
+  (if(>(string-length input) 7)
+     (cond ;then condition
+       [(equal? (substring input 0 8) "#definev")(display "variable ")]
+       [(equal? (substring input 0 8) "#definef")(display "function ")]
+       );end cond
+     (void);else void
+     );end if
   (cond
     [(equal? input "#clear") (ClearStacks)];clears the memory ;COMPLETE
     [(equal? input "#exit")(set! quit #t)];exits the program ;COMPLETE
-    
-    [else (display "Invalid input ")] ;if none of the above, invalid
+    [else (void)]
     );end cond
+    
+
   
   (if (equal? quit #t); if #exit, quit.
       (display "Quitting UofL")
