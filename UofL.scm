4f22189aeb94248171211d7aaa16cf85021420c2
Justin <lego.man.123@hotmail.com>
2018-04-09 23:11:35 -0600

    Variable and Function Creation
    implemented #definefunc
    implemented #definevari
    implemented storage of functions and variables
    implemented helper functions for searching and returning string indecies and variable values

--- UofL.scm
+++ UofL.scm
@@ -13,6 +13,51 @@
   (set! globalVariStack '()) ;empties global variables
   )
 
+;DefineVariable
+(define (defineVariable input)
+  (define index 0)
+  (set! index(stringSearch input #\space 0));start searching from position 0
+  (set! globalVariStack
+        (append globalVariStack
+                (list (substring input 0 index);variable name
+                      (substring input (+ 1 index) (string-length input));variable type
+                      )
+                );end append to globalVari
+        );end set
+  (set! globalVariStack (append globalVariStack (list 0)));set value of variable to default as 0
+   (display globalVariStack)
+  );end define
+
+;Get Value From Variable
+(define (getValue varName index)
+  (if (equal? varName (list-ref globalVariStack index));check if the current iterated variable name is right
+      (list-ref globalVariStack (+ index 2));if yes, return the value of the variable
+      (getValue varName (+ index 3)));else go to the next variable name in the list
+  )
+
+;define function
+(define (defineFunction input)
+   (define index 0)
+  (set! index(stringSearch input #\space 0));start searching from position 0
+  (set! globalFuncStack
+        (append globalFuncStack
+                (list (substring input 0 index);Function name
+                      (substring input (+ 1 index) (string-length input));variable type
+                      )
+                )
+        );done set function name
+  (display globalFuncStack)
+  );END define function
+
+
+
+;Search for character in a string
+(define (stringSearch input findChar index)
+  (if (equal? (string-ref input index) findChar) ;if input's char at position index is equal to findChar
+      index                                    ;return index position
+      (stringSearch input findChar (+ index 1))) ;else iterate through
+  )
+
 ;UofL 
 (define (UofL . x);creates a function without input
 (define input "") ;input
@@ -20,8 +65,8 @@
   (set! input (read-line));reads input
   (if(>(string-length input) 7)
      (cond ;then condition
-       [(equal? (substring input 0 8) "#definev")(display "variable ")]
-       [(equal? (substring input 0 8) "#definef")(display "function ")]
+       [(equal? (substring input 0 8) "#definev")(defineVariable (substring input 12 (string-length input)))]
+       [(equal? (substring input 0 8) "#definef")(defineFunction (substring input 12 (string-length input)))]
        );end cond
      (void);else void
      );end if
@@ -39,3 +84,4 @@
      );endif
     );END OF UOFL
 (UofL)
+
